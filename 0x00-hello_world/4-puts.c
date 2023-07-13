c0090207415f41137dc9325dd49eece91bb8a1e1
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-13 07:51:09 -0700

    some edits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+  * main - puts a statment
+  * Return: 0
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+  3   * main - prints a statement
+  4   * Return: 0
+  5   */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
