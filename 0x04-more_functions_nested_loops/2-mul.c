87afb249c7b456499512ae0ed79d1dbc788059fc
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-20 09:21:39 -0700

    first chunk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * mul - multiplication of a, b
+  * @a: number 1
+  * @b: number 2
+  * Return: the mult
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_numbers - print numbers
+  * Return: none
+  *
+  */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
