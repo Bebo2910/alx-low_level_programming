ceec54c64b237b561c887a3cd75a8cefd03db573
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-20 19:21:41 -0700

    the hardest

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+/**
+  * _sqrt - function
+  * @x: input number
+  * Return: square root of x
+  */
+
+double _sqrt(double x)
+{
+	float sqrt, tmp;
+
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
+	{
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
+  * largest_prime_factior - the largest divisor
+  * @num: the number
+  * Return: the largest
+  */
+
+void largest_prime_factior(long int num)
+{
+	int prmN, largest;
+
+	while (num % 2 == 0)
+	{
+		num = num / 2;
+	}
+	for (prmN = 3; prmN <= _sqrt(num); prmN += 2)
+	{
+		while (num % prmN == 0)
+		{
+			num = num / prmN;
+			largest = prmN;
+		}
+	}
+
+	if (num > 2)
+	{
+		largest = num;
+	}
+
+	printf("%d\n", largest);
+}
+
+/**
+  * main - the main
+  * Return: 0
+  */
+
+int main(void)
+{
+	largest_prime_factor(612852475143);
+
+	return (0);
+}
