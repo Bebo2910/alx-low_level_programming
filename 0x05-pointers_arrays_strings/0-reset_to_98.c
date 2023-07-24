8231800006c921493eccffe657e6e2055baa52a1
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-23 23:45:17 -0700

    class B

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
   * reset_to_98 - function to change the value
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -12,6 +12,6 @@ void swap_int(int *a, int *b)
 	int d;
 
 	d = *a;
-	* a = *b;
+	*a = *b;
 	*b = d;
 }
