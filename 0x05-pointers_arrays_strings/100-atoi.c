e3589108259d97af7376a9585ae6e31c7a83e158
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-24 23:34:50 -0700

    advanced 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _atoi - print number in string
+  * Return: result
+  * @s: string of a number
+  *
+  *
+  */
+
+int _atoi(char *s)
+{
+	int i, result = 0, dir = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/*to make a check for the sign*/
+		if (s[i] == '-')
+		{
+			dir = -dir;
+		}
+		/*make a check if it is a number*/
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = (result * 10 + (s[i] - '0')) * dir;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (result);
+}
