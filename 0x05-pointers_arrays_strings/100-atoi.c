3f9d11eae6c51383b8068971d16e5029d0eaf42c
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-24 23:56:14 -0700

    advanced time

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,7 +24,11 @@ int _atoi(char *s)
 		{
 			result = result * 10 + (s[i] - '0');
 		}
-		else if ((s[i] <= '0' || s[i] >= '9') && (s[i] != '+' || s[i] != '-'))
+		else if (s[i] == '+' || s[i] == '-')
+		{
+			continue;
+		}
+		else
 		{
 			break;
 		}
