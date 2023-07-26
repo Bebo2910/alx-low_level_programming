80db0d893461acbc98d789a93a1572e848f43220
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-26 08:54:38 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *_strcat - function to merge strings
+  * @dest: string of merge
+  * @src: string to be merged
+  * Return: merged string
+  *
+  */
+char *_strcat(char *dest, char *src)
+{
+	/*finding the length of the string*/
+	int i, l, count;
+
+	for (i = 0; src[i] != '\0'; i++)
+		;
+
+	for (l = 0; dest[i] != '\0'; l++)
+		;
+	for (count = 0; count < i; count++)
+	{
+		dest[l + count] = src[count];
+	}
+
+	return (dest);
+
+
