d92dfc6c598b8371dcc0acdc9ca45cb9bba25f93
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-31 08:48:04 -0700

    task 4 e

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,15 +9,21 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	int count = 0, i, j;
+	int i, j;
+	unsigned int count = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[i] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				count++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (count);
 			}
 		}
 	}
