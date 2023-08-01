852e4ab1253cab4402eb21b02d810104e7f27081
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-31 19:29:12 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_diagsums - print diagsums
+  * @a: the array
+  * @size: the size of the array
+  * Return: none.
+  *
+  */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i][i];
+	}
+
+	for (j = size; j > 0; j--)
+	{
+		sum2 += a[j - 1][j - 1];
+	}
+
+	printf("%d,%d\n", sum1, sum2);
+}
