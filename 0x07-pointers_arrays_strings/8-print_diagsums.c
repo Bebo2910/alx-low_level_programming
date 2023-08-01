29d039064a5e6928878c86980114058e0d35481b
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-31 19:35:07 -0700

    trust 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,12 +16,12 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i][i];
+		sum1 += a[i * size + i];
 	}
 
 	for (j = size; j > 0; j--)
 	{
-		sum2 += a[j - 1][j - 1];
+		sum2 += a[j * size + (size - j - 1)];
 	}
 
 	printf("%d,%d\n", sum1, sum2);
