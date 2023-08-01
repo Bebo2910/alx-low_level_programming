89860c729fc936f9f8fb2f9ff0f15be9dc0aa2ac
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-07-31 19:41:19 -0700

    task hilarious

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * set_string - hilarious
+  * @s: the pointer of pointers
+  * @to: the value
+  * Return: none
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
