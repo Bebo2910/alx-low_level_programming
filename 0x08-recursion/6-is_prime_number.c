1bcf57c97152fbb4ce2bf16592367b0d3773b510
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-02 05:07:56 -0700

    finalllllll

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+  * flag_is - function to test the number
+  * @n: number to test
+  * @i: mod number
+  * Return: 0 or 1
+  */
+
+int flag_is(int n, int i)
+{
+	int flag = 0;
+
+	if (i <= n / 2)
+	{
+		if (n % i == 0)
+		{
+			flag = 1;
+			return (flag);
+		}
+		else
+		{
+			flag_is(n, i + 1);
+		}
+	}
+	else
+	{
+		return (flag);
+	}
+}
+
+/**
+  * is_prime_number - function to find prime numbers
+  * @n: number to determine
+  * Return: 0 or 1.
+  *
+  *
+  */
+
+int is_prime_number(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (flag_is(n, 2));
+	}
+}
