df7b22f6e2b5cdcd9665542b0dc827c8ae1db662
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-02 03:11:17 -0700

    task 0 e

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,13 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\0');
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
-	_putchar(s);
-	*s += 1;
-	_puts_recursion(*s);
+	else
+	{
+		_putchar('\n');
+	}
 }
