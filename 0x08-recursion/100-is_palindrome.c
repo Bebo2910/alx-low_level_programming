1557b3e14ff4ee1b2351f1f39aa863970ef9e5e7
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-03 01:32:15 -0700

    task adv 1 final ISA

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,21 +3,43 @@
 /**
   * is_right - the function to test
   * @s: the string
+  * @size: size of string
   * @i: the number of array
   * Return: 1 0r 0
   */
-int is_right(char *s, int i)
+int is_right(char *s, int size, int i)
 {
-	if (s[i + 1])
+	if (i < size / 2)
 	{
-		if (s[i] != s[-1 - i])
+		if (s[i] != s[size - 1 - i])
 		{
 			return (0);
 		}
-		return (is_right(s, i + 1));
+		return (is_right(s, size, i + 1));
 	}
 	return (1);
 }
+
+/**
+  * size - finding size of string
+  * @s: the string
+  * Return: size
+  *
+  */
+
+int size(char *s)
+{
+	int n = 0;
+
+	if (*s != '\0')
+	{
+		n += size((s + 1)) + 1;
+	}
+
+	return (n);
+}
+
+
 /**
   * is_palindrome - the function to simi
   * @s: string to test
@@ -28,5 +50,8 @@ int is_right(char *s, int i)
 
 int is_palindrome(char *s)
 {
-	return (is_right(s, 0));
+	int n;
+
+	n = size(s);
+	return (is_right(s, n, 0));
 }
