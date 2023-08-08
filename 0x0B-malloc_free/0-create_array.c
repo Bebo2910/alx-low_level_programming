c0ac1d2aad4843958571db691483ce5ec5253099
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-08 07:12:38 -0700

    task 0 e

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
   * create_array - function to make array
@@ -9,13 +10,13 @@
   */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
-
 	int i;
 	char *s = malloc(size);
+
+	if (size == 0 || s == 0)
+	{
+		return (0);
+	}
 
 	for (i = 0; i < size; i++)
 	{
