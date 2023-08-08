cd9d799ad56e9b28fb3d11b0052027e43960f37b
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-08 16:34:17 -0700

    task 1 advanced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strlen - function to find lenght
+  * @s: string to find lenght
+  * Return: strlen
+  */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+  * argstostr - function to make
+  * @ac: number of columns
+  * @av: pointer to pointer
+  * Return: 0 or string
+  *
+  */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i = 0, j = 0, totalsize = 0, index = 0, n = 0;
+
+	if (ac == 0 || av == 0)
+	{
+		return (0);
+	}
+
+	for (; i < ac; i++, totalsize++)
+	{
+		totalsize += _strlen(av[i]);
+	}
+
+	s = malloc(sizeof(char) * totalsize + 1);
+
+	for (; n < ac; n++)
+	{
+		for (; av[n][j] != '\0'; j++, index++)
+		{
+			s[index] = av[n][j];
+		}
+		s[index] = '\n';
+		index++;
+	}
+
+	return (s);
+}
