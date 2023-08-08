5f06a3f6741c272e24bcb3333b34d7e969464da1
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-08 07:24:05 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - fucntion to duplicate string
+  * @str: string to plain
+  * Return: 0 or string
+  *
+  */
+char *_strdup(char *str)
+{
+	char *dip = malloc(sizeof(str));
+
+	if (dip == 0)
+	{
+		return (0);
+	}
+
+	int i = 0;
+
+	while (str[i])
+	{
+		dip[i] = str[i];
+		i++;
+	}
+
+	return (dip);
+}
