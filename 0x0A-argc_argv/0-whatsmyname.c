88e4102025061c7196016f0630989f7b2e538a4e
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-09 19:41:19 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - print argument name
+  * @argc: the len
+  * @argv: the string
+  * Return: 0
+  *
+  */
+int main (int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
