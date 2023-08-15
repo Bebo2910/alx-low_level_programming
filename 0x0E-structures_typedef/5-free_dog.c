f4d2df23ebbe31199556c0f946fc60dc06c13f2e
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-14 17:19:03 -0700

    taks final

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+  * free_dog - free a dog
+  * @d: the dog to be freed
+  * Return: void
+  *
+  */
+void free_dog(dog_t *d)
+{
+	free(dog->name);
+	free(dog->age);
+	free(dog->owner);
+	free(dog);
+}
