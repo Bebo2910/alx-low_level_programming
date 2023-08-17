13ecdfd81fd8b06ec9b4e39f7dc6fa9810d411f4
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-17 14:58:18 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,13 +16,13 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	va_start (ap, n);
+	va_start(ap, n);
 
 	for (; i < n; i++)
 	{
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 	}
-	va_end (ap);
+	va_end(ap);
 
 	return (sum);
 }
