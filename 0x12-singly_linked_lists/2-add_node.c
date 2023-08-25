14148c2331756053086bef20178d515d92b112d3
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-24 18:02:30 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+  *_strlen - func
+  * @s: char
+  * Return: return string chars
+  *
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
+  * add_node - fucn
+  * @head: head
+  * @str: string
+  * Return: string
+  *
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!new || !head)
+	{
+		return (NULL);
+	}
+
+	if (str)
+	{
+		new->str = strdup(str);
+		if (!new->str)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->len = _strlen(new->str);
+	}
+
+	new->next = *head;
+	*head = new;
+	return (new);
+}
