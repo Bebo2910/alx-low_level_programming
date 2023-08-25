4f787b75c58b177e08faf95f7911bb9377174d77
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-24 17:43:32 -0700

    silly

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ int _strlen(char *s)
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t new = malloc(sizeof(list_t));
+	list_t *new = malloc(sizeof(list_t));
 
 	if (!new || !head)
 	{
