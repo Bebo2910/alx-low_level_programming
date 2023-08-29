225507f8fbf7143a206079b24a85d9269b245724
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-29 08:21:34 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *buffer = head, *new = malloc(sizeof(listint));
+	listint_t *buffer = head, *new = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
 	if (!new || !head)
