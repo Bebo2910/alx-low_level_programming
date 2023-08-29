98d9b38d74fd0a2d9d4379000f437f94b4488328
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-29 08:04:26 -0700

    task 7 af

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - function
+  * @head: new
+  * @index: the index desired
+  * Return: head
+  *
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *buffer = head, *new = malloc(sizeof(listint));
+	unsigned int i = 0;
+
+	if (!new || !head)
+	{
+		return (NULL);
+	}
+
+	while (i < index && buffer)
+	{
+		buffer = buffer->next;
+	}
+
+	if (!buffer)
+	{
+		return (NULL);
+	}
+
+	new->n = buffer->n;
+	new->next = head;
+	head = new;
+	return (head);
+}
