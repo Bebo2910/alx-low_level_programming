432dd089b1c9116d7fd0795337254de4a53358d2
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-29 07:34:21 -0700

    free not free

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,8 +13,13 @@ int pop_listint(listint_t **head)
 	listint_t *node;
 	int i;
 
+	if (!head || !*head)
+	{
+		return (0);
+	}
+
 	i = (*head)->n;
-	node = *head;
+	node = *head
 	*head = (*head)->next;
 	free(node);
 
