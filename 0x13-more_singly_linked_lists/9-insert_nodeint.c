06056b8c180f7b0f65460c65694714a65bac5dd8
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-29 17:31:19 -0700

    task 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	while (i < idx && buffer)
+	while (i =< idx && buffer)
 	{
 		buffer = buffer->next;
 		i++;
