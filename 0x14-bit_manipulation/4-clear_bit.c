22eed0cc6588f326cd544d445c9af67774da9874
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-31 14:01:43 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * clear_bit - function to clear bit
+  * @n: n
+  * @index: index
+  * Return: 1 or -1
+  *
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	*n = ((0 << index) | *n);
+
+	return (1);
+}
