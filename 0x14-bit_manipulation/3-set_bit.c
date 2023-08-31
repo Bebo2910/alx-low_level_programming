7796f0d805276ae71be81ce7b8d696b41eea9417
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-31 13:58:59 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,14 +10,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int new = 0;
-
 	if (index > 63)
 	{
 		return (-1);
 	}
 
-	new = (1 << index) | n;
-
-	return (new);
+	*n = ((1 << index) | *n);
+	return (1);
 }
