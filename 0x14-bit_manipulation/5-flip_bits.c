9f0b85914577c1224980841c1fead022339bb0d0
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-31 14:19:41 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * flip_bits - function
+  * @n: num 1
+  * @m: num 2
+  * Return: return
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int buffer, current;
+	unsigned int count;
+
+	buffer = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = (buffer >> i) & 1;
+
+		if (current == 1)
+		{
+			count++;
+		}
+	}
+
+	return (count);
+}
