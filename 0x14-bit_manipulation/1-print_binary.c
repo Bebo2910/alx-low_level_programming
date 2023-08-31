1d04c4e7f8a3b7df3ffac3b06b04fa7e0071ebaa
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-31 08:55:15 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * print_binary - function to print binary
+  * @n: the number
+  * Return: void
+  *
+  *
+  */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int head;
+	int count = 0, i;
+
+	for (i = 63; i >= 0; i--)
+	{
+		head = n >> i;
+		if (head & 1L)
+		{
+			_putchar('1');
+			count++
+		}
+		else if (count)
+		{
+			_putchar('0');
+		}
+	}
+	if (!count)
+	{
+		_putchar('0');
+	}
+}
