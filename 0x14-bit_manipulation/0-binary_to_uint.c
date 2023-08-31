7535e46ca241fefcdd425e4d3db2e2513867e9cc
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-31 08:49:31 -0700

    taks 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,23 +12,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec;
-	int index, i;
+	int i;
 
-	for (index = 0; b[index] != '\0'; index++)
+	if (!b)
 	{
-		;
+		return (0);
 	}
 
-	for (i = 0; i < index; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
-		if (b[i] == '1')
-		{
-			dec += pow(2, index - i - 1);
-		}
+		dec = dec * 2 + (b[i] - '0');
 	}
 
 	return (dec);
