9e72ac594783ba2f9810fd3de90defdb75f4f707
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-12 21:07:41 +0300

    insert in place

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *buffer = *h;
+	int i = 0;
+
+	if (!new || !h)
+	{
+		return (new ? free(new), NULL : NULL);
+	}
+
+	if (idx == 0)
+	{
+		free(new);
+		add_dnodeint(&(*h), n);
+		return (*h);
+	}
+
+	for (; i < idx - 1; i++)
+	{
+		if (!(*h) && i == idx - 2)
+		{
+			free(new);
+			add_dnodeint_end(&(*h), n);
+			return (*h);
+		}
+		buffer = buffer->next;
+	}
+	new->n = n;
+	new->prev = buffer;
+	new->next = buffer->next;
+	buffer->next->prev = new;
+	buffer->next = new;
+	return (*h);
+}
