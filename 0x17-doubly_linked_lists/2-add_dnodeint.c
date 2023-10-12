5bd52477cf63aacc8465978ee4ae26867333b279
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-12 15:08:18 +0300

    new node added

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,6 +5,7 @@
  * @n: the new number
  * Return: adderss on success
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
@@ -19,5 +20,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	(*head) = new;
 
-	return (new);
+	return (*head);
 }
