221352677d71893ad55c59fb5f8ac0488260733d
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-20 16:29:59 +0300

    advanced 1

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,26 +1,67 @@
-#include "main.h"
-
-int sum(int a, int b)
+#include <stdio.h>
+/**
+ * sum - sum the numbers
+ * @a: the first int
+ * @b: the second int
+ * Return: the sum
+ */
+int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
+
+/**
+ * div - div the numbers
+ * @a: the first int
+ * @b: the second int
+ * Return: the the division
+ */
+
 
 float div(int a, int b)
 {
-	return a / b;
+	if (b == 0)
+	{
+		printf("Error: the under mod is zero");
+		return (-1);
+	}
+	return (a / b);
 }
+
+/**
+ * sub - sum the numbers
+ * @a: the first int
+ * @b: the second int
+ * Return: the sum
+ */
+
 
 int sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
+
+/**
+ * mul - sum the numbers
+ * @a: the first int
+ * @b: the second int
+ * Return: the sum
+ */
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
+
+/**
+ * mod - sum the numbers
+ * @a: the first int
+ * @b: the second int
+ * Return: the mod
+ */
+
 
 int mod(int a, int b)
 {
-	return a % b;
+	return (a % b);
 }
